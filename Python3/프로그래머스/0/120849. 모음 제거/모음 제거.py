73313881708370458cba3a122504310707a6b99c
Selena <113491089+songmac@users.noreply.github.com>
2026-02-05 22:19:37 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

--- Python3/프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ Python3/프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,2 @@
 def solution(my_string):
-    answer = ""
-    for s in my_string:
-        if s in ["a", "e", "i", "o", "u"]:
-            continue
-        answer += s
-    return answer
+    return "".join([i for i in my_string if not(i in "aeiou")])
